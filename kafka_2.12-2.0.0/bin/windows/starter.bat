145ad0c7a8d8f559e3981351283cbca2e3eb5f9d
David Petrocelli <dmpetrocelli@gmail.com>
2018-08-13 18:23:43 -0300

    Organizing repo

--- kafka_2.12-2.0.0/bin/windows/starter.bat
+++
@@ -1,1 +0,0 @@
-D:\kafka_2.12-2.0.0\bin\windows\zookeeper-server-start.bat D:\kafka_2.12-2.0.0\config\zookeeper.properties & D:\kafka_2.12-2.0.0\bin\windows\kafka-server-start.bat D:\kafka_2.12-2.0.0\config\server.properties
