085a3ed095fe68563bfcd0534fe093889ce6d76a
dpetrocelli <38283586+dpetrocelli@users.noreply.github.com>
2018-11-20 17:15:29 -0300

    Add files via upload

---
+++ copyremote.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo copy to 10.4.6.12
+sshpass -p "alumno" scp -oStrictHostKeyChecking=no -r /media/alumno1/BV/ThesisDoc/ alumno1@10.4.6.12:/home/david/test
+echo copy to 10.4.6.13
+sshpass -p "alumno" scp -oStrictHostKeyChecking=no -r /media/alumno1/BV/ThesisDoc/ alumno1@10.4.6.13:/home/david/test
+echo copy to 10.4.6.21
+sshpass -p "alumno" scp -oStrictHostKeyChecking=no -r /media/alumno1/BV/ThesisDoc/ alumno1@10.4.6.21:/home/david/test
+echo copy to 10.4.6.22
+sshpass -p "alumno" scp -oStrictHostKeyChecking=no -r /media/alumno1/BV/ThesisDoc/ alumno1@10.4.6.22:/home/david/test
+echo copy to 10.4.6.23
+sshpass -p "alumno" scp -oStrictHostKeyChecking=no -r /media/alumno1/BV/ThesisDoc/ alumno1@10.4.6.23:/home/david/test
+echo copy to 10.4.6.31
+sshpass -p "alumno" scp -oStrictHostKeyChecking=no -r /media/alumno1/BV/ThesisDoc/ alumno1@10.4.6.31:/home/david/test
+echo copy to 10.4.6.32
+sshpass -p "alumno" scp -oStrictHostKeyChecking=no -r /media/alumno1/BV/ThesisDoc/ alumno1@10.4.6.32:/home/david/test
+echo copy to 10.4.6.33
+sshpass -p "alumno" scp -oStrictHostKeyChecking=no -r /media/alumno1/BV/ThesisDoc/ alumno1@10.4.6.33:/home/david/test
+echo copy to 10.4.6.41
+sshpass -p "alumno" scp -oStrictHostKeyChecking=no -r /media/alumno1/BV/ThesisDoc/ alumno1@10.4.6.41:/home/david/test
+echo copy to 10.4.6.42
+sshpass -p "alumno" scp -oStrictHostKeyChecking=no -r /media/alumno1/BV/ThesisDoc/ alumno1@10.4.6.42:/home/david/test
+echo copy to 10.4.6.43
+sshpass -p "alumno" scp -oStrictHostKeyChecking=no -r /media/alumno1/BV/ThesisDoc/ alumno1@10.4.6.43:/home/david/test
